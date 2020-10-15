e9a8e600ec396eb33909bcda15ef037f6e3aadfc
Elderberg <noway@creamoff.xyz>
2020-10-15 14:44:20 +0200

    OpenHomo-9: add initial spring project

---
+++ src/test/java/com/openhomo/api/OpenhomoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openhomo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenhomoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
