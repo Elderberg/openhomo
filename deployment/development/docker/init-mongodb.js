fd3c32198988def6f4ffc26c318b7c1babdd6802
Wanheda <->
2020-11-08 17:46:36 +0100

    OpenHomo-16: add database, collection and document

--- deployment/development/docker/init-mongodb.js
+++ deployment/development/docker/init-mongodb.js
@@ -1,3 +1,5 @@
+//docs.mongodb.com/manual/reference/method/
+
 db.createUser({
     user: 'test-user',
     pwd: '1234',
@@ -7,4 +9,15 @@ db.createUser({
             db: 'test-database',
         },
     ],
+});
+
+//authentication and creating Database + Collection
+db.auth('test-user', '1234');
+db.createCollection('testcollection');
+
+//insert Document(s) in Collection named "testcollection"
+db.testcollection.insert({
+    id: "315327d0-d195-474d-a86b-9a615ba40249",
+    state: 100,
+    name: 'Test Node'
 });
